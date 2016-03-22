28686b21d3a8ef87f57f9cb5e4e257d18434124a
Андрей Антонов <a.antonov@tradedealer.ru>
2016-03-22 18:22:58 +0500

    bower install script

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Etfostra\PageLayoutBundle\Composer;
+
+
+/**
+ * Class ScriptHandler
+ * @package Etfostra\PageLayoutBundle\Composer
+ */
+class ScriptHandler
+{
+    public static function installGridStackJs()
+    {
+        $currentDirectory = getcwd();
+        $bundleDirectory = realpath(__DIR__.'/../');
+        $status = null;
+        $output = array();
+
+        chdir($bundleDirectory);
+        exec('bower install', $output, $status);
+        chdir($currentDirectory);
+
+        if ($status) {
+            throw new \RuntimeException("Running 'bower install' failed with $status\n");
+        }
+    }
+}
