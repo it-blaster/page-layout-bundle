c77a0d5113e9b3872689280bfda4e2f638fec523
Anton Smolin <anton2smolin@gmail.com>
2017-04-05 17:43:59 +0500

    fix

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -12,30 +12,12 @@ class ScriptHandler
 {
     public static function installGridStackJs()
     {
+        $bundleDirectory = realpath(__DIR__.'/..');
 
-        $currentDirectory = getcwd();
-        $bundleDirectory = realpath(__DIR__.'/../');
-        /*
-        $status = null;
-        $output = array();
-
-        chdir($bundleDirectory);
-        */
         $command = 'bower install --allow-root -F';
         $process = new Process($command, $bundleDirectory);
         $process->run();
-        //exec('bower install --allow-root -F', $output, $status);
-        /*
-        chdir($currentDirectory);
 
-        foreach($output as $line) {
-            echo $line."\n";
-        }
-
-        if ($status) {
-            throw new \RuntimeException("Running 'bower install' failed with $status\n");
-        }
-*/
         if (!$process->isSuccessful()) {
             throw new \RuntimeException("Running 'bower install' failed from directory ".$bundleDirectory.". Descritption error: ".$process->getErrorOutput());
         }
